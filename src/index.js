01cfdaf3afb51e7e7554b995fd81bc3e2f222cfa
Rajat Singh <iamrajatsingh2@gmail.com>
2021-06-01 23:18:41 +0530

    dev done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+/**
+ * This is a basic routing app using ReactJs & react-router-dom
+ */
